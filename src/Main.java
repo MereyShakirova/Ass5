831f040ec674929dd9eb8374eb95cc6c62d72027
Shakirova Merei <sh.merei05@gmail.com>
2023-05-23 01:42:14 +0600

    Commit29

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
 public class Main {
 public static void main(String[] args){
- BST<Integer, String>tree = new BST<>();
- tree.put(0, "P");
- tree.put(3, "R");
- tree.put(4, "O");
- tree.put(1, "M");
- tree.put(7, "I");
- tree.put(2, "S");
- tree.put(5, "E");
- tree.delete(0);
- for (BST.KeyValue<Integer, String> entry: tree){
-  System.out.println("key iss " + entry.getKey() + ", " + "value is " + entry.getValue() + ".");
+ BST<Integer, String> bst = new BST<>();
+ bst.put(0, "P");
+ bst.put(3, "R");
+ bst.put(4, "O");
+ bst.put(1, "M");
+ bst.put(7, "I");
+ bst.put(2, "S");
+ bst.put(5, "E");
+ System.out.println("Elements in the BST class:");
+ for (BST.KeyValue<Integer, String> entry: bst){
+  System.out.println("Keys: " + entry.getKey() + "; " + "Values: " + entry.getValue() + ";");
  }
- System.out.println("after the deletion:");
 
- for (BST.KeyValue<Integer, String> entry: tree){
-  System.out.println("key iss " + entry.getKey() + ", " + "value is " + entry.getValue() + ".");
+ bst.delete(0);
+ bst.delete(6);
+ System.out.println("After the deletion method:");
+ for (BST.KeyValue<Integer, String> entry: bst){
+  System.out.println("Keys: " + entry.getKey() + "; " + "Values: " + entry.getValue() + ";");
  }
- System.out.println("Size of tree is "+ tree.size() + ".");
+
+ int key = 2;
+ String value = bst.get(key);
+ System.out.println("Value for key " + key + ": " + value);
+
+
+ System.out.println("Size of tree is "+ bst.size() + ".");
 }
 }
