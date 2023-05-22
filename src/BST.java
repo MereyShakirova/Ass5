4a3092c862b62b9dc1b700e372381210da1bbf99
Shakirova Merei <sh.merei05@gmail.com>
2023-05-22 22:32:09 +0600

    Commit5

--- src/BST.java
+++ src/BST.java
@@ -21,6 +21,16 @@ private Node put(Node node, K key, V val){
         size++;
         return new Node(key, val);
     }
-    int cmp = key.compare
+    int cmp = key.compareTo(node.key);
+    if(cmp < 0){
+        node.left = put(node.left, key, val);
+    }
+    else if(cmp > 0){
+        node.right = put(node.right, key, val);
+    }
+    else{
+        node.val = val;
+    }
+    return node;
 }
 }
