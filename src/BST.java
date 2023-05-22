70d9e1a8fd07f1190aea9aa7f91d128fd3fbef68
Shakirova Merei <sh.merei05@gmail.com>
2023-05-22 22:23:44 +0600

    Commit3

--- src/BST.java
+++ src/BST.java
@@ -2,6 +2,16 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Stack;
 public class BST <K extends Comparable<K>, V> implements Iterable<BST.KeyValue<K,V>> {
-
+private Node root;
+private int size;
+private class Node{
+    private K key;
+    private V val;
+    private Node left, right;
+    public Node(K key, V val){
+        this.key = key;
+        this.val = val;
+    }
+}
 
 }
