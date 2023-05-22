5199cbde94d2d2485b4fe097bf00ec6021659668
Shakirova Merei <sh.merei05@gmail.com>
2023-05-23 01:25:18 +0600

    Commit27

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
 public static void main(String[] args){
- BST<String, Integer>tree = new BST<>();
- tree.put("P", 0);
- tree.put("R", 3);
- tree.put("O", 4);
- tree.put("M", 1);
- tree.put("I", 8);
- tree.put("S", 2);
- tree.put("E", 5);
- for (BST.KeyValue<String, Integer> entry: tree){
+ BST<Integer, String>tree = new BST<>();
+ tree.put(0, "P");
+ tree.put(3, "R");
+ tree.put(4, "O");
+ tree.put(1, "M");
+ tree.put(7, "I");
+ tree.put(2, "S");
+ tree.put(5, "E");
+ tree.delete(0);
+
+ for (BST.KeyValue<Integer, String> entry: tree){
   System.out.println("key iss " + entry.getKey() + ", " + "value is " + entry.getValue() + ".");
  }
  System.out.println("Size of tree is "+ tree.size() + ".");
