5bbe2d6b73c84f46cd6524f97f093a6f866aa923
Shakirova Merei <sh.merei05@gmail.com>
2023-05-22 21:47:09 +0600

    Commit2

--- src/BST.java
+++ src/BST.java
@@ -1,4 +1,7 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Stack;
-public class
+public class BST <K extends Comparable<K>, V> implements Iterable<BST.KeyValue<K,V>> {
+
+
+}
